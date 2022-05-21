3c290686b5e01c00ea8f3234ac0c3106377b6bc6
vrvivek <vivekvr123@gmail.com>
2022-05-21 12:35:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SimpleForm from './Components/SimpleForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <SimpleForm />
       </header>
     </div>
   );
---
+++ src/Components/SimpleForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SimpleForm extends Component {
+  render() {
+    return (
+      <div>SimpleForm</div>
+    )
+  }
+}
